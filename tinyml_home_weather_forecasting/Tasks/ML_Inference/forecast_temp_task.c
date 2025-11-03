1d3f9846b2bf2ada0b4d7110b0f505636994a029
Rishi Latchmepersad <latchmepersad@gmail.com>
2025-11-03 19:46:33 -0400

    adding tasks for ML inference

---
+++ tinyml_home_weather_forecasting/Tasks/ML_Inference/forecast_temp_task.c
@@ -0,0 +1,8 @@
+/*
+ * forecast_temp_task.c
+ *
+ *  Created on: Nov 3, 2025
+ *      Author: rishi_latchmepersad
+ */
+
+
---
+++ tinyml_home_weather_forecasting/Tasks/ML_Inference/forecast_temp_task.h
@@ -0,0 +1,13 @@
+/*
+ * forecast_temp_task.h
+ *
+ *  Created on: Nov 3, 2025
+ *      Author: rishi_latchmepersad
+ */
+
+#ifndef ML_INFERENCE_FORECAST_TEMP_TASK_H_
+#define ML_INFERENCE_FORECAST_TEMP_TASK_H_
+
+
+
+#endif /* ML_INFERENCE_FORECAST_TEMP_TASK_H_ */
