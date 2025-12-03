68f4639c9fa0d860b8618b66050685a9794b7b04
Rishi Latchmepersad <latchmepersad@gmail.com>
2025-12-03 07:50:55 -0400

    new rnn

--- tinyml_home_weather_forecasting/X-CUBE-AI/App/forecast_temp_ml_model_data.h
+++ tinyml_home_weather_forecasting/X-CUBE-AI/App/forecast_temp_ml_model_data.h
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    forecast_temp_ml_model_data.h
   * @author  AST Embedded Analytics Research Platform
-  * @date    2025-11-30T13:51:42-0400
+  * @date    2025-12-03T07:44:26-0400
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * Copyright (c) 2025 STMicroelectronics.
@@ -32,7 +32,7 @@ AI_DEPRECATED
 AI_API_DECLARE_BEGIN
 
 
-extern const ai_u64 s_forecast_temp_ml_model_weights_array_u64[2039];
+extern const ai_u64 s_forecast_temp_ml_model_weights_array_u64[523];
 
 
 
